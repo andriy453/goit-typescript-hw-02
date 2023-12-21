68fb453337ac9156faac2a896a790062cf09217d
andriy <evicandrij@gmail.com>
2023-12-21 19:20:37 +0200

    comit

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -4,6 +4,6 @@
 */
 type Gender = "male" | "female"
 
-const myGender : Gender   
+let  myGender : Gender   
 
 export {};
--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,13 +11,16 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare<T extends AllType>(
+  top: Pick<T, "name" | "color">,
+  bottom: Pick<T, "position" | "weight">
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
